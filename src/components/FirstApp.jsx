2df641e38dfcb4d6a8b09e14ce1d3c4c60450d5a
brandonruizmora <brandon_ruizmor@outlook.es>
2022-06-22 11:34:54 -0500

    Create first app counter and added styles css

---
+++ src/components/FirstApp.jsx
@@ -0,0 +1,42 @@
+
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const FirstApp = ({ value }) => {
+
+  const [counter, setCounter] = useState(value);
+
+  const handleClickAdd = function () {
+    setCounter(counter + 1);
+  }
+
+  const handleClickSubstract = function () {
+    setCounter(counter - 1);
+  }
+
+  const handleClickReset = function () {
+    setCounter(value);
+  }
+
+  return (
+    <div className='container'>
+      <div className='center-text'>
+        <h1>CounterApp</h1>
+        <h2>{counter}</h2>
+        <button type='button' onClick={handleClickAdd}>+1</button>
+        <button type='button' onClick={handleClickSubstract}>-1</button>
+        <button type='button' onClick={handleClickReset}>reset</button>
+      </div>
+    </div>
+  )
+}
+
+FirstApp.propTypes = {
+  value: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+  value: 0
+}
+
+export default FirstApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import FirstApp from './components/FirstApp';
+import './components/FirstApp.css';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <FirstApp />
     </React.StrictMode>
 )
