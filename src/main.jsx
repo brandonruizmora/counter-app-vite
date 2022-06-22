be5d7bc2b30f742d7a33fbd2ef9d2adfab093d52
brandonruizmora <brandon_ruizmor@outlook.es>
2022-06-21 20:50:53 -0500

    created first component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-    return (<h1>Hola Mundo</h1>);
-}
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
